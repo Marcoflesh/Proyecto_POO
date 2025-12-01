2eec73d36ff9ca1802d0f8fa4a800eea3c3b560d
Marcoflecha <istacu29@gmail.com>
2025-11-30 22:07:42 -0600

    Prototipos clases
    agregar diferentes clases para proxima actualizacion

---
+++ proyecti/Bebida.h
@@ -0,0 +1,18 @@
+#include <string>
+#define BEBIDA_H
+#ifndef BEBIDA_H
+using namespace std;
+
+class Bebida {
+
+    protected:
+        float precio;
+        float porcentajeAlcohol;
+        float iva;
+        float descuento;
+        string tipo;
+        string marca;   
+    
+}
+
+#endif 
---
+++ proyecti/Taberna.h
@@ -0,0 +1,11 @@
+#define TABERNA_H
+#ifndef TABERNA_H
+#include "Bebida.h"
+#include <string>
+using namespace std;
+
+class Taberna : Bebida {
+
+}
+
+#endif 
